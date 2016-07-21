cfa13aa45762e9a33454f8058dfc2a9267f61073
Zoe Love <zoelove.zle@gmail.com>
2016-07-22 00:43:15 +0530

    Completed basic shotgun adn machine gun scripts . They are inherited from a weapon class

--- Assets/Scripts/Functions/Math/MathS.cs
+++ Assets/Scripts/Functions/Math/MathS.cs
@@ -6,10 +6,12 @@ namespace Squad.MathS
 	public class MathS {
 		public static Vector3 RotateAroundAxis(float radius , float rotateAngle , float rotateAxis , Vector3 center) { // the angles are in radians
 			var rot_x = radius * Mathf.Cos(rotateAngle) ;
-			var x = center.x + rot_x * Mathf.Cos(rotateAxis) ;
+
+			var x = center.x + rot_x * Mathf.Cos(-rotateAxis) ;
 			var y = center.y + radius * Mathf.Sin(rotateAngle) ;
-			var z = center.z + rot_x * Mathf.Sin(rotateAxis) ;
+			var z = center.z + rot_x * Mathf.Sin(-rotateAxis) ;
 			return new Vector3(x,y,z);
 		}
+		public readonly static float PI2 = 6.2831853071795f ;
 	}
 }
---
+++ Assets/Scripts/Guns/Shotgun.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+// using System.Collections;
+using Squad.MathS ;
+public class Shotgun : Weapon {
+	[RangeAttribute(0.001f , 1f)] public float GunPointRadius = 1f ;
+	
+	public void Start() {
+		NextShot = 0.5f ;
+	}
+	public void Update() {
+		if(Time.time > NextShot) { // This is overall same for all weapons 
+			NextShot += ShotSpan ;
+			Fire() ;
+			ShakeGun() ;
+		}
+	}
+	public void Fire() { // MathS function is custom and can be used by including the library by Squad.MathS
+	
+		// var pos = new Vector3() ;
+		// var angle = 0f ;
+		// var shotRadius = 0f ;
+		for(var i = 0 ; i < MaxShots ; i++) {
+			var angle = Random.Range(0,MathS.PI2) ;
+			var shotRadius = Random.Range(0,GunPointRadius);
+			var pos = MathS.RotateAroundAxis(shotRadius , angle , transform.rotation.eulerAngles.y * Mathf.PI/180 , transform.position) ;
+			var clone = Instantiate(object_MuzzleFlash , pos , Quaternion.identity) ;
+			Destroy(clone , MuzzleSpan) ;
+		}
+	}
+	public void ShakeGun() { // This is function to be used after shots to shake the gun .
+
+	}
+	public void OnDrawGizmos() {
+		Gizmos.DrawWireSphere(transform.position , GunPointRadius) ;
+	}
+}
